1cdd0fdd10d0f169d65f30851b89d59430431096
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-21 00:59:13 +0530

    modified name of the all programs...

--- Assigment-6/02_last_digit.c
+++ Assigment-6/02_last_digit.c
@@ -7,6 +7,7 @@ int main()
     scanf("%d", &num);
     // To find last digit of given number
     last_digit = num % 10; // Remender will disply [ex=123] then only 3 will disply
+    
     printf("last Digit is =%d", last_digit);
 
     return 0;
