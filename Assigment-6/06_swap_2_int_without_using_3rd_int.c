62bb03c49b319184c7d3ff6b255d079db0445b82
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-21 03:30:42 +0530

    swap programs

---
+++ Assigment-6/06_swap_2_int_without_using_3rd_int.c
@@ -0,0 +1,23 @@
+// Write a program to swap values of two int variables without using third variable
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the First Number A:");
+    scanf("%d", &a);
+
+    printf("Enter the second Number B:");
+    scanf("%d", &b);
+
+    printf("Before swapping::");
+    printf("a=%d and b=%d", a, b);
+
+    // Swapping Number
+    a = a + b; // a=10, b=20    a+b=(10+20)=30;
+    b = a - b; // a=30, b=20  b=a-b=(30-20)=10
+    a = a - b; // a=30, b=10  a-b=(30-10)=20
+
+    printf("\n After Swapping \n");
+    printf("Number after Swap A=%d and B=%d", a, b);
+    return 0;
+}
