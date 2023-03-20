fc8be5f96630c16fd7c890429f5b336223b09d5a
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-21 03:20:21 +0530

    swap values of two int variables

---
+++ Assigment-6/05_swap_2_int.c
@@ -0,0 +1,21 @@
+// Write a program to swap values of two int variables
+#include <stdio.h>
+int main()
+{
+    int a, b, temp;
+    printf("Enter the value of A:");
+    scanf("%d", &a);
+
+    printf("Enter the value of B:");
+    scanf("%d", &b);
+
+    printf("Number Before swapp: A=%d and B=%d \n",a,b);
+
+  // Condition To Swap the Number
+    temp = a; // a=5 temp=5
+    a = b;    // a=5 b=10
+    b = temp; // temp=5 b=5
+    printf("Number After swapp: A=%d and B=%d",a,b);
+
+    return 0;
+}
