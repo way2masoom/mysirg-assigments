95e883ec10468409d31da201a3192e499a20465c
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-03-20 23:58:37 +0530

    this code is write according to question

--- Assigment-6/last_digit.c
+++ Assigment-6/last_digit.c
@@ -2,20 +2,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2,num3,lastDigit;
+    int num, last_digit;
     printf("Enter the Number:");
-    scanf("%d", &num1);
-
-    printf("Enter the Number:");
-    scanf("%d", &num2);
-
-    printf("Enter the Number:");
-    scanf("%d", &num3);
-
+    scanf("%d", &num);
     // To find last digit of given number
-    lastDigit = num1 % 10; // Remender will disply [ex=123] then only 3 will disply
-
-    printf("last Digit is =%d", lastDigit);
+    last_digit = num % 10; // Remender will disply [ex=123] then only 3 will disply
+    printf("last Digit is =%d", last_digit);
 
     return 0;
 }
