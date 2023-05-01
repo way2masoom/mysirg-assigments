7c656bb5e130e665c5481e8fe9cc0248567bbfe3
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-01 16:55:11 +0530

    program to input a num and a digit and append the digit in number

---
+++ Assigment-6/08_pro.c
@@ -0,0 +1,10 @@
+// Write a program to input a number from the user and also input a digit. 
+// Append a digit in the number and print the resulting number. 
+// (Example - number=234 and digit=9 then the resulting number is 2349)
+
+#include<stdio.h>
+int main()
+{
+
+    return 0;
+}
