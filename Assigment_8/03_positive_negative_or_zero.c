c46253592c5ddf2da140cf182e76a233fba2c2d9
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-04 06:27:54 +0530

    program to find positive, negative or zero.

--- Assigment_8/03_positive_negative_or_zero.c
+++ Assigment_8/03_positive_negative_or_zero.c
@@ -1,2 +1,22 @@
-// Write a program to check whether a given number 
+// Write a program to check whether a given number
 // is positive, negative or zero.
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number Please :");
+    scanf("%d", &num);
+
+    // condition to check number is positive, Negative or Zero
+    if (num >= 0)
+    {
+        printf("Positive Number");
+    }
+    else if (num <= 0)
+    {
+        printf("Number is Negative or Zero");
+    }
+
+    return 0;
+}
