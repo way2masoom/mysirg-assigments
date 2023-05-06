292f4e28771f0ba77fbb10b44146720f4a8252fb
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-06 20:18:01 +0530

    program to print the first 10 even natural numbers in reverse order.

---
+++ Assigment_9/05_first_10_even_natrul_num_reverse.c
@@ -0,0 +1,13 @@
+//  Write a program to print the first 10 even natural numbers in reverse order.
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    while ((i >= 2))
+    {
+        printf("Even Natural REVERSE Number=%d\n", i);
+        i -= 2;
+    }
+
+    return 0;
+}
