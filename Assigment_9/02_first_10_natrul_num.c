4580514a918c5dda9f84f218935029b4abb422f1
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-06 20:03:57 +0530

    Write a program to print the first 10 even natural numbers. and program to print the first 10 natural numbers in reverse order

---
+++ Assigment_9/02_first_10_natrul_num.c
@@ -0,0 +1,12 @@
+// write a program to print the first 10 natural numbers.
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("\n Natural Number =%d", i);
+        i++;
+    }
+    return 0;
+}
---
+++ Assigment_9/04_first_10_even_natrul_num.c
@@ -0,0 +1,6 @@
+// Write a program to print the first 10 even natural numbers.
+#include<stdio.h>
+int main()
+{
+ return 0;
+}
