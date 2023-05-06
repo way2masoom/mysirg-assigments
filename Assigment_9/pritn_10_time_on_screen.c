8ca3cfe4a42a9785c78c0c6825988741cf36147e
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-06 19:48:31 +0530

    program to print MySirG 10 times on the screen.

---
+++ Assigment_9/pritn_10_time_on_screen.c
@@ -0,0 +1,13 @@
+// Write a program to print MySirG 10 times on the screen.
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("\n MySirG");
+        i++;
+    }
+
+    return 0;
+}
