1e4d796d09813bc8b118c9fe4336cf1e95294013
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-06 09:03:37 +0530

    Program remove last digit of given number

---
+++ Assigment_8/removed_last_digit.c
@@ -0,0 +1,16 @@
+// WAP  how to remove last digit
+#include <stdio.h>
+int main()
+{
+    int num, last_digit;
+    printf("Enter the Number:");
+    scanf("%d", &num);
+    // To remove last digit
+    // when we divided any num with 10 we get first 2 number only and LAST digit will remove
+    //[ex = 234/10] then only 23 will display
+    last_digit = num / 10;
+
+    printf("last Digit is =%d", last_digit);
+
+    return 0;
+}
