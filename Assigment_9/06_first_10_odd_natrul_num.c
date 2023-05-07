68e2951ff1a7ddfef7cc2f35eceb8eaf0ab9f6a8
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2023-05-07 08:38:00 +0530

    a program to print the first 10 odd natural numbers && EVEN number and also to find SQUARE AND CUBES of the number

---
+++ Assigment_9/06_first_10_odd_natrul_num.c
@@ -0,0 +1,13 @@
+// Write a program to print the first 10 odd natural numbers
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("Odd Natural Number=%d\n", i);
+        i += 2;
+    }
+
+    return 0;
+}
---
+++ Assigment_9/08_first_10_cubes_natrul_num.c
@@ -0,0 +1,13 @@
+// Write a program to print the cubes of first 10 natural numbers
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("Square of first 10 Natural %d NUmber=%d\n", i, i * i * i);
+        i++;
+    }
+
+    return 0;
+}
